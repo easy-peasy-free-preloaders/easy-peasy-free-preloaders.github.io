d06c1108ab144f1fef9806b12ee5d1c7070cc23c
maksimpecerin <lordoys@gmail.com>
2019-10-25 20:47:54 +0300

    Add v-for and hover

--- js/main.js
+++ js/main.js
@@ -1,6 +1,30 @@
 new Vue({
   el: '#app',
   data: {
-    message: 'adfga'
+    activeIndex: null,
+    items: [
+      {},
+      {
+        spans: 4
+      },
+      {},
+      {},
+      {},
+      {},
+      {},
+      {},
+      {},
+      {},
+      {}
+    ]
+  },
+  methods: {
+    sort(s, index) {
+      this.activeIndex = index;
+      if(s === this.currentSort) {
+        this.currentSortDir = this.currentSortDir==='asc'?'desc':'asc';
+      }
+      this.currentSort = s;
+    }
   }
 });
